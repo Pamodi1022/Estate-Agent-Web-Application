47149fdf5a38c5c148145835d2497aa21f194e76
Pamodi1022 <pamodipansiluni1022@gmail.com>
2024-12-22 22:41:42 +0530

    feat: update logo text in Navbar component to reflect new branding

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
         <Link to="/" onClick={() => setMenuActive(false)}>
           <img src={logo} alt="Logo" className="logo-img" />
         </Link>
-        <h1 className="logo-text">Ticket.lk</h1>
+        <h1 className="logo-text">QuickNest</h1>
       </div>
       <button className="menu-toggle" onClick={toggleMenu}>
         ☰
