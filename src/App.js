11b2f0ba9355439ebf0997d5307a8ed88ab52b2a
Pamodi1022 <pamodipansiluni1022@gmail.com>
2024-12-24 22:57:51 +0530

    feat: update routing in App component to include Home and Advertisements pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-// filepath: /c:/Users/ASUS/Desktop/ACS-cw/estate_agent_webapplication/src/App.js
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Signup from './Components/Signup';
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 import SearchBar from './Components/SearchBar';
 import SearchForm from './Components/SearchForm';
+import Advertisements from './Components/Advertisements';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Advertisements />
       <div>
         <Routes>
-          <Route path="/" element={<SearchBar />} />
+          <Route path="/" element={<Home />} />
           <Route path="/Signup" element={<Signup />} />
-          <Route path="/search" element={<SearchForm />} />
+          <Route path="/search" element={<SearchBar />} />
+          <Route path="/ads" element={<Advertisements />} />
+          <Route path="/searchform" element={<SearchForm />} />
         </Routes>
       </div>
     </Router>
