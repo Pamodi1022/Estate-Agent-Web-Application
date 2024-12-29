dea6d890ccd898a34a8cda42ba1179b80b1d3956
Pamodi1022 <pamodipansiluni1022@gmail.com>
2024-12-30 02:01:06 +0530

    fix: clean up formatting and remove unnecessary line breaks in Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,7 @@
-// HomePage.js
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../Styles/Home.css'; // Add your CSS file for styling
-import back from '../Components/background.png'; // Import the image
+import back from '../Components/background.png'; // Import the image
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -15,23 +14,21 @@ const HomePage = () => {
     <div className="home-page-container">
       <div className="text-container">
         <h1 className="home-title">Real Estate</h1>
-        <h1 className="home">Agency</h1><br></br>
+        <h1 className="home">Agency</h1><br />
         <p className="home-description">
-        Discover your perfect home with us - your ultimate destination for comfort and convenience. Whether you're searching for a cozy apartment, a spacious family home, or a luxurious retreat, we offer a curated selection of properties designed to meet your unique lifestyle. Let us help you find a place where you can truly feel at home.
+          Discover your perfect home with us - your ultimate destination for comfort and convenience. Whether you're searching for a cozy apartment, a spacious family home, or a luxurious retreat, we offer a curated selection of properties designed to meet your unique lifestyle. Let us help you find a place where you can truly feel at home.
         </p>
       </div>
       
-        
       <div className="circle-container"></div>
 
       <div className="image-container">
-        <img src={back} alt="Banner" className="home-image" />
-        <div className="arrow-container" onClick={handleArrowClick}>
-          <span className="arrow">&#8594;</span> {/* You can use an arrow icon here */}
-        </div>
+        <img src={back} alt="Banner" className="home-image" />
+          <div className="arrow-container" onClick={handleArrowClick}>
+            <span className="arrow">&#8594;</span> {/* You can use an arrow icon here */}
+          </div>
       </div>
     </div>
-    
   );
 };
 
