98b86c99a919a65b499d67893f6951e5f8810d85
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-01-03 21:38:47 +0530

    feat: integrate drag-and-drop functionality and favourites context in App component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { DndProvider } from 'react-dnd';  // Import DndProvider
+import { HTML5Backend } from 'react-dnd-html5-backend'; 
 
-import Signup from './Components/Signup';
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import SearchBar from './Components/SearchBar';
-import SearchForm from './Components/SearchForm';
-import PropertiesList from './Components/PropertiesList';
-import ResultsPage from './Components/ResultsPage';
+import { FavouritesProvider } from "./Context/FavouritesContext";
+import Signup from "./Components/Signup";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import SearchBar from "./Components/SearchBar";
+import SearchForm from "./Components/SearchForm";
+import PropertiesList from "./Components/PropertiesList";
+import ResultsPage from "./Components/ResultsPage";
+import FavList from "./Components/FavList";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Signup" element={<Signup />} />
-          <Route path="/search" element={<SearchBar />} />
-          <Route path="/ads" element={<PropertiesList />} />
-          <Route path="/searchform" element={<SearchForm />} />
-          <Route path="/result" element={<ResultsPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <DndProvider backend={HTML5Backend}>  {/* Wrap with DndProvider */}
+    <FavouritesProvider>
+      <Router>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/search" element={<SearchBar />} />
+            <Route path="/ads" element={<PropertiesList />} />
+            <Route path="/searchform" element={<SearchForm />} />
+            <Route path="/result" element={<ResultsPage />} />
+            <Route path="/favlist" element={<FavList />} />
+          </Routes>
+        </div>
+      </Router>
+    </FavouritesProvider>
+    </DndProvider>
   );
 }
 
