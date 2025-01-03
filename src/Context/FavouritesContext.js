c4a10a441c8fc339c5b4b22693f9aee809376065
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-01-03 23:57:07 +0530

    feat: add functionality to add and remove properties from favourites

--- src/Context/FavouritesContext.js
+++ src/Context/FavouritesContext.js
@@ -9,12 +9,14 @@ export const useFavourites = () => {
 export const FavouritesProvider = ({ children }) => {
   const [favourites, setFavourites] = useState([]);
 
+  // Add a property to favourites, but only if it's not already in the list
   const addToFavourites = (property) => {
     if (!favourites.some((fav) => fav.id === property.id)) {
       setFavourites((prev) => [...prev, property]);
     }
   };
 
+  // Remove a property from favourites
   const removeFromFavourites = (propertyId) => {
     setFavourites((prev) => prev.filter((fav) => fav.id !== propertyId));
   };
