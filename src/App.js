38e81bd9c77619b97681270e298370278058dce1
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-01-04 22:11:00 +0530

    feat: add Footer component and improve layout structure in App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { DndProvider } from 'react-dnd';  // Import DndProvider
-import { HTML5Backend } from 'react-dnd-html5-backend'; 
+import { DndProvider } from "react-dnd"; // Import DndProvider
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 import { FavouritesProvider } from "./Context/FavouritesContext";
 import Signup from "./Components/Signup";
@@ -12,26 +12,28 @@ import SearchForm from "./Components/SearchForm";
 import PropertiesList from "./Components/PropertiesList";
 import ResultsPage from "./Components/ResultsPage";
 import FavList from "./Components/FavList";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <DndProvider backend={HTML5Backend}>  {/* Wrap with DndProvider */}
-    <FavouritesProvider>
-      <Router>
-        <Navbar />
-        <div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/search" element={<SearchBar />} />
-            <Route path="/ads" element={<PropertiesList />} />
-            <Route path="/searchform" element={<SearchForm />} />
-            <Route path="/result" element={<ResultsPage />} />
-            <Route path="/favlist" element={<FavList />} />
-          </Routes>
-        </div>
-      </Router>
-    </FavouritesProvider>
+    <DndProvider backend={HTML5Backend}> {/* Wrap with DndProvider */}
+      <FavouritesProvider>
+        <Router>
+          <Navbar /> {/* Navbar will remain on top of all pages */}
+          <div style={{ flex: 1 }}> {/* Main content area */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/search" element={<SearchBar />} />
+              <Route path="/ads" element={<PropertiesList />} />
+              <Route path="/searchform" element={<SearchForm />} />
+              <Route path="/result" element={<ResultsPage />} />
+              <Route path="/favlist" element={<FavList />} />
+            </Routes>
+          </div>
+          <Footer /> {/* Footer remains at the bottom */}
+        </Router>
+      </FavouritesProvider>
     </DndProvider>
   );
 }
